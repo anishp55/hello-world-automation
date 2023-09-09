f1d337b3695e80ce6fdc205789df30d8d13b8d24
Anish Patel <anishs dot online dot bills at gmail>
2023-09-09 03:25:51 -0400

    added env var for prod deployment

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -1,13 +1,23 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+
 
 @RestController
 public class HelloController {
 
+    @Autowired private Environment environment;    
     @GetMapping("/hello")
     public String hello() {
-        return "Hello from Azure Spring Apps, today is a greay day\n";
+        String value = "Hello from Azure Spring Apps, today is a greay day";
+        if (environment.getProperty("env") == null)
+            return value;
+        else
+            return value + " " + environment.getProperty("env");
+            
     }
 }
