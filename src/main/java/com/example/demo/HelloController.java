b25cc1eb9f9ea18193e7f438ef971fca57b28fd5
Anish Patel <anishs dot online dot bills at gmail>
2023-12-20 00:58:43 -0700

    update message

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -1,5 +1,7 @@
 package com.example.demo;
 
+import java.time.LocalTime;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,10 +15,10 @@ public class HelloController {
     @Autowired private Environment environment;    
     @GetMapping("/")
     public String hello() {
-        String value = "Hello from TAP on AWS";
+        String value = "Hello from TAP on ASAe";
         if (environment.getProperty("env") != null)
             value+= " , today is a great day for" + environment.getProperty("env");
-        
+            value+= "<br>"  + LocalTime.now();
         return value;
 
     }
